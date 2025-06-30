43d4a86c6f87757c83c8ac871f5c3921b8ff3870
VIA2NEY <kouameangeV11@gmail.com>
2025-06-30 19:02:20 +0000

    Adding web form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# ☝️ Au lieu de app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 from app import routes
