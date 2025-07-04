43c3ef3b2d67e4d2e2e3725533fb516c2d1dd470
VIA2NEY <kouameangeV11@gmail.com>
2025-07-04 22:13:32 +0000

    Adding pagination feature

--- config.py
+++ config.py
@@ -17,3 +17,6 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['graceful.marten.wtte@letterhaven.net']
+
+    
+    POSTS_PER_PAGE = 10
